c13d5192d95c964d9d32e9a8a21fff6bf42bdc39
Simon Hicks <me@simonhicks.org>
2011-02-12 17:43:26 +0700

    updated description

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shicks/debug "1.0.0"
-  :description "A collection of basic debugging functions"
+(defproject org.clojars.shicks/debug "1.0.0"
+  :description "A collection of basic debugging tools"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
